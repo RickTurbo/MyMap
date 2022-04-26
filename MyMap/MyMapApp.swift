b84ba97d20013dde50ba51400c049b9796e59c4c
kojima rikuto <kojikoji7765@icloud.com>
2022-04-26 17:43:03 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 児島　陸斗 on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
