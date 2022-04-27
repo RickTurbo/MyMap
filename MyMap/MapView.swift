c6b888ad07cf391131eb957a13cb9ac4b0646fc3
kojima rikuto <kojikoji7765@icloud.com>
2022-04-27 21:05:13 +0900

    マップアプリ完成

--- MyMap/MapView.swift
+++ MyMap/MapView.swift
@@ -11,6 +11,7 @@ import MapKit
 struct MapView: UIViewRepresentable {
 
     let searchKey: String
+    let mapType: MKMapType
 
     func makeUIView(context: Context) -> MKMapView {
         MKMapView()
@@ -19,12 +20,44 @@ struct MapView: UIViewRepresentable {
     func updateUIView(_ uiView: MKMapView, context: Context) {
 
         print(searchKey)
+
+        uiView.mapType = mapType
+
+        let geocoder = CLGeocoder()
+
+        geocoder.geocodeAddressString(
+            searchKey,
+            completionHandler: { (placemarks, error) in
+
+                if let unwrapPlacemarks = placemarks ,
+                   let firstPlacemark = unwrapPlacemarks.first ,
+                   let location = firstPlacemark.location {
+
+                    let targetCordinate = location.coordinate
+
+                    print(targetCordinate)
+
+                    let pin = MKPointAnnotation()
+
+                    pin.coordinate = targetCordinate
+
+                    pin.title = searchKey
+
+                    uiView.addAnnotation(pin)
+
+                    uiView.region = MKCoordinateRegion(
+                        center: targetCordinate,
+                        latitudinalMeters: 500.0,
+                        longitudinalMeters: 500.0
+                    )
+                }
+            })
     }
 
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView(searchKey: "東京タワー")
+        MapView(searchKey: "東京タワー", mapType: .standard)
     }
 }
